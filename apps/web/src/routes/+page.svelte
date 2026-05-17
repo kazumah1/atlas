6c175469f01e324eabab20cc444018343a9f3662
Kazuma <162946059+kazumah1@users.noreply.github.com>
2026-05-16 18:13:27 -0700

    feat: add api client and types

---
+++ apps/web/src/routes/+page.svelte
@@ -0,0 +1,44 @@
+<h1>Papers</h1>
+<h2>search</h2>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<input type="text" placeholder="Search" />
+<style>
+    :global(h1) {
+        font-size: 3em;
+        color: #5a5a5a;
+    }
+    :global(h2) {
+        font-size: 2em;
+        color: #8f8f8f;
+    }
+    :global(h3) {
+        font-size: 1em;
+        font-weight: bold;
+        color: #ffffff;
+    }
+    :global(p) {
+        font-size: 1em;
+        color: #ffffff;
+    }
+    :global(a) {
+        font-size: 1em;
+        color: blue;
+        text-decoration: underline;
+        color: #7f7f7f
+    }
+    :global(body) {
+        background-color: #1a1a1a;
+        margin-left: 20px;
+        margin-right: 10px;
+    }
+    :global(input) {
+        color: #ffffff;
+        background-color: #1a1a1a;
+        border: 1px solid #5a5a5a;
+        border-radius: 5px;
+        padding: 5px;
+        font-size: 1em;
+        width: 500px;
+        height: 30px;
+    }
+</style>
---
+++ apps/web/src/routes/papers/[id]/+page.svelte
@@ -0,0 +1,1 @@
+<h1>page for paper 1</h1>
