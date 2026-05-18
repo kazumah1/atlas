539c75e358112e779b8d1500b29d6766b61903f3
Kazuma <162946059+kazumah1@users.noreply.github.com>
2026-05-17 17:15:08 -0700

    add CORS middleware

--- apps/api/app.py
+++ apps/api/app.py
@@ -3,9 +3,18 @@ from pydantic import BaseModel
 from typing import Union, Optional, List
 from datetime import datetime
 from apps.api.helpers import get_sorted_results, fetch_papers_from_ids
+from infra.postgres import db_get_paper
+from fastapi.middleware.cors import CORSMiddleware
 
 
-app = FastAPI("papers api server")
+app = FastAPI(title="papers api server")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
 
 @app.get("/health")
 def health():
@@ -13,8 +22,8 @@ def health():
 
 
 @app.get("/search/{search_query}")
-def search(search_query: str, date_from: Optional[timestamp], date_to: Optional[timestamp], tags: Optional[List[str]]):
-    result_ids = get_sorted_results(query=search_query, date_from=date_from, date_to=date_to, tags=tags)
+def search(search_query: str, date_from: Optional[datetime] = None, date_to: Optional[datetime] = None, tags: Optional[List[str]] = None, page: int = 0, limit: int = 20):
+    result_ids = get_sorted_results(query=search_query, date_from=date_from, date_to=date_to, tags=tags, page=page, limit=limit)
 
     results = fetch_papers_from_ids(result_ids)
 
@@ -22,3 +31,7 @@ def search(search_query: str, date_from: Optional[timestamp], date_to: Optional[
             "results":results
     }
 
+@app.get("/papers/{paper_id}")
+def get_paper(paper_id: str):
+    paper_records = db_get_paper(paper_id)
+    return paper_records[0]
